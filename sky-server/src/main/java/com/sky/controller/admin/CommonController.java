c7b13af64165000f995031f37843057fe2045d09
liuweiguan1 <2912359263@xiaomi.com>
2024-07-05 23:48:01 +0800

    完成了菜品分页查询、增加以及删除功能。(yml删除掉了阿里云密钥相关的信息)

--- sky-server/src/main/java/com/sky/controller/admin/CommonController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -1,6 +1,7 @@
 package com.sky.controller.admin;
 
 import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -10,15 +11,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/admin/common")
 @Slf4j
 @Api(tags = "通用接口")
 public class CommonController {
+    private final AliOssUtil aliOssUtil;
+
+    public CommonController(AliOssUtil aliOssUtil) {
+        this.aliOssUtil = aliOssUtil;
+    }
+
     @PostMapping("/upload")
     @ApiOperation("文件上传")
-    public Result<String> upload(MultipartFile file) {
+    public Result<String> upload(MultipartFile file) throws IOException {
         log.info("文件上传:{}",file);
-        return null;
+        String originalFilename = file.getOriginalFilename();
+        String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+        String objectName = UUID.randomUUID().toString()+extension;
+        String filePath = aliOssUtil.upload(file.getBytes(),objectName);
+        return Result.success(filePath);
     }
 }
